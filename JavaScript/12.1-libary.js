e49e7fca7f71cd89cd55b9d81d3e1ffce8a490b2
sharonatok <sharona.tok@gmail.com>
2022-04-19 03:32:21 +0300

    adding 16.1

---
+++ JavaScript/12.1-libary.js
@@ -0,0 +1,25 @@
+// JavaScript – Libary
+// The following exercise contains the following subjects:
+//  Array Methods
+// Instructions
+// Write a JavaScript function that receives the following library object as an
+// input and displays the books that can be read (the reading status is true).
+// Log the book name, author name and reading status .
+
+var library = [
+{
+author: "Bill Gates",
+title: "The Road Ahead",
+readingStatus: true
+},
+{
+author: "Steve Jobs",
+title: "Walter Isaacson",
+readingStatus: true
+},
+{
+author: "Suzanne Collins",
+title: "Mockingjay: The Final Book of The Hunger Games",
+readingStatus: false
+}
+];
